886081bfdc3685f612892c778955103931831e0e
Gaizka Carmona <67479691+GaizFenix@users.noreply.github.com>
2025-11-11 15:41:42 +0100

    Change get_balance to return 'dinero' instead of 'balance'

--- wallet.py
+++ wallet.py
@@ -6,7 +6,7 @@ class Wallet:
         self.balance = self.balance + val
 
     def get_balance(self):
-        return self.balance
+        return self.dinero
 
     def remove_balance(self, val):
         self.balance = self.balance - val
